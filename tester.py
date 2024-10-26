2ea3046eacd46d069a7e61e133417bb165b05a3b
Aditya Sharma <adusharma2004@gmail.com>
2024-10-26 14:25:31 -0400

    test

--- tester.py
+++ tester.py
@@ -1,17 +1,32 @@
 import os
+from dotenv import load_dotenv
 from openai import OpenAI
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
+load_dotenv()
+
+key =os.getenv("OPEN_AI_KEY")
+client = OpenAI(api_key=key)
 
 chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Say this is a test",
+            "content": "explain 2d kinematics in great detail",
         }
     ],
-    model="gpt-3.5-turbo",
+    model="o1-preview",
 )
+
+prompt = (chat_completion.choices[0].message.content)
+
+code = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": prompt + "explain all of this using manim to create visualtions for each concept with transition frm each concept to another using clear and precise drawing and texts that do not overlap."
+        }
+    ],
+    model="o1-preview"
+)
+
+print(code.choices[0].message.content)
