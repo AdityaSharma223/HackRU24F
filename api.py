250742a9bb353094285daf6de07f071f916d8a8d
Oqawa12 <140027540+Oqawa12@users.noreply.github.com>
2024-10-26 19:17:20 -0400

    Update api.py

--- api.py
+++ api.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 import base64
-import os
 from manim import *
+import os
+import video
 
 app = FastAPI()
 
@@ -20,16 +21,15 @@ def encode_image(image_path):
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 @app.post("/video")
-async def query_AI(question: str = Form(...), image_file: UploadFile = File(...)):
-    image_path = f"temp_{image_file.filename}"
-    with open(image_path, "wb") as buffer:
-        buffer.write(await image_file.read())
-    base64_image = encode_image(image_path)
-    video = feed_AI(question, image_path)
+async def query_AI(question: str = Form(...), image_file: UploadFile = File(None)):
+    if image_file:
+        image_path = f"temp_{image_file.filename}"
+        with open(image_path, "wb") as buffer:
+            buffer.write(await image_file.read())
+        base64_image = encode_image(image_path)
+    file_id = video.generate_manim_visualization(question)
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    output_video_path = os.path.join(current_directory, 'output_videos', f'{file_id}.mp4')
+    print(output_video_path)
     #os.remove(image_path)
-    return FileResponse(video)
-    # Return a video file based on the question and image if needed
-
-def feed_AI(question, image_b64):
-    print(f"ChatGTP Recieved: {question} and {image_b64}")
-    return None
+    return FileResponse(output_video_path)
